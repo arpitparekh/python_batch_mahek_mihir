5383d78d8732e1f7549905232a0b86bbdf8a002c
arpitparekh <arpitparekh9@gmail.com>
2025-01-21 12:08:18 +0530

    first commit

---
+++ hello_world.py
@@ -0,0 +1,47 @@
+print("Hello World")
+a = 12
+b = 13
+print(a+b)
+
+# comment
+"""
+multi line comment
+
+variables
+datatype
+there are no arrays in python
+operators (symbols)
+conditional statements // if else elif // when
+loops // while // for
+strings
+functions // block of code that runs when we called it
+
+oopc
+class
+object
+constrcutor
+inheritance
+polymorphism
+encapsulation
+abstraction
+
+file hanlding
+exception handling
+modules
+
+decorators
+generators
+
+"""
+
+def maruFunction():
+  print("maru")
+
+
+maruFunction()
+maruFunction()
+maruFunction()
+maruFunction()
+
+
+
