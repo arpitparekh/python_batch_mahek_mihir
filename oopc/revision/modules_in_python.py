6186ffac78a0c4e268423141f7a75e77f51cea5d
arpitparekh <arpitparekh9@gmail.com>
2025-02-28 11:43:08 +0530

    system modules

--- oopc/revision/modules_in_python.py
+++ oopc/revision/modules_in_python.py
@@ -8,7 +8,7 @@
 # sv.outerfunction()()
 
 
-from polymorphism import marufunction,taruFuntion
+from polymorphism import marufunction,taruFuntion   # usedefine module
 # from polymorphism import *   # import all the functions from polymorphism module
 
 marufunction(1,2,3)
@@ -17,3 +17,8 @@ taruFuntion(name="Bascom",age=20)
 
 from scope_of_variable import outerfunction
 outerfunction()()
+
+
+import sys     # system module
+print(sys.path)
+print(sys.version)
